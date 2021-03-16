e34b93a64db17f96707db9baba6ee2283f6f0472
Mena Azir <AzirM6513@users.noreply.github.com>
2021-03-16 14:32:51 -0400

    part2 2.6-2.9

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    if (valueEmpty(personObject)) {
+      alert("please fill all fields");
+      return;
+    }
+
+    if (personExists(personObject)) {
+      alert(`${personObject.name} was already added to phonebook`);
+      return;
+    }
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const personExists = (personObject) => {
+    const personArray = persons.map((person) => person.name);
+    return personArray.includes(personObject.name);
+  };
+
+  const valueEmpty = (personObject) => {
+    return !(personObject.name && personObject.number);
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearchChange} />
+      </div>
+      <h2>add new contact information</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {filteredPersons.map((person) => (
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
